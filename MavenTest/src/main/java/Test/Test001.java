cd219f280eed1f50a9548923603c60f9d53747ef
MCprogrem <1239629948@qq.com>
2018-05-31 11:59:18 +0800

    adder

---
+++ MavenTest/src/main/java/Test/Test001.java
@@ -0,0 +1,11 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Test001 {
+	@Test
+	public void clear() {
+		System.out.println("我是好人");
+	}
+
+}
---
+++ MavenTest/src/main/java/com/haotest/MavenTest/App.java
@@ -0,0 +1,13 @@
+package com.haotest.MavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
